23088521db46bfff346c7484f8ea169e6e1745ee
Mehul Shukla <shuklamehul686@gmail.com>
2026-03-15 11:04:19 +0530

    Add files via upload

---
+++ Prac1_ai.py
@@ -0,0 +1,20 @@
+def generate_magic_square(n):
+    magic_square = [[0 for _ in range(n)] for _ in range(n)]
+    i, j = 0, n // 2
+    num = 1
+
+    while num <= n * n:
+        magic_square[i][j] = num
+        num += 1
+        new_i, new_j = (i - 1) % n, (j + 1) % n
+
+        if magic_square[new_i][new_j]:
+            i = (i + 1) % n
+        else:
+            i, j = new_i, new_j
+    return magic_square
+
+n = 3   
+square = generate_magic_square(n)   
+for row in square:
+    print(row)
